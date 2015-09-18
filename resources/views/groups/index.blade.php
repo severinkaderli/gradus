f271ed79b51a6ed4dfe803775bc6801f21fbaee2
Severin Kaderli <severin.kaderli@gmail.com>
2015-09-18 18:32:00 +0200

    Worked on the index styles

--- resources/views/groups/index.blade.php
+++ resources/views/groups/index.blade.php
@@ -7,28 +7,28 @@
 @section('content')
 
     @forelse($groups as $group)
-        <div class="group cell cell--3">
+        <div class="group cell cell--12">
             <header class="group__header">
                 <h2>{{$group->name}}</h2>
             </header>
             <div class="group__content">
                 @foreach($group->subjects as $subject)
                     <div class="subject">
-                        <header>
+                        <header class="subject__header">
                             <h3>{{$subject->name}}</h3>
                         </header>
                         <div class="subject__content">
                             1.1, 1.3, 5.5, 6 (+)
                         </div>
                         <footer class="subject__footer">
-                            Fach zählt doppelt!
+                            Fach z&auml;hlt doppelt!
                         </footer>
                     </div>
 
                 @endforeach
             </div>
             <footer class="group__footer">
-                Add subject
+                <a href="">Add Subject</a>
             </footer>
         </div>
 
