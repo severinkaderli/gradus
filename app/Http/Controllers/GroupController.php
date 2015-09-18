3c1f5439c9faa0989c85cd914a7839a7bd075ea8
Severin Kaderli <severin.kaderli@gmail.com>
2015-09-18 17:43:48 +0200

    Worked on the group index page

--- app/Http/Controllers/GroupController.php
+++ app/Http/Controllers/GroupController.php
@@ -2,10 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use App\Group;
-use App\User;
 use App\Http\Requests;
-use App\Http\Controllers\Controller;
+use Auth;
+
 
 class GroupController extends Controller
 {
@@ -16,7 +15,8 @@ class GroupController extends Controller
      */
     public function index()
     {
-        return view('groups.index');
+        $groups = Auth::user()->groups()->latest()->get();
+        return view('groups.index', compact('groups'));
     }
 
 }
--- resources/views/groups/index.blade.php
+++ resources/views/groups/index.blade.php
@@ -6,5 +6,33 @@
 
 @section('content')
 
-    Hello world
+    @forelse($groups as $group)
+        <div class="group cell cell--3">
+            <header class="group__header">
+                <h2>{{$group->name}}</h2>
+            </header>
+            <div class="group__content">
+                @foreach($group->subjects as $subject)
+                    <div class="subject">
+                        <header>
+                            <h3>{{$subject->name}}</h3>
+                        </header>
+                        <div class="subject__content">
+                            1.1, 1.3, 5.5, 6 (+)
+                        </div>
+                        <footer class="subject__footer">
+                            Fach zählt doppelt!
+                        </footer>
+                    </div>
+
+                @endforeach
+            </div>
+            <footer class="group__footer">
+                Add subject
+            </footer>
+        </div>
+
+    @empty
+        No groupos
+    @endforelse
 @stop
