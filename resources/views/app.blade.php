e4834bd196525fbb1c6f96c86803bb7c90e0a973
Severin Kaderli <severin.kaderli@gmail.com>
2015-09-19 15:06:25 +0200

    Added ability to delete groups

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -92,6 +92,7 @@
         <!-- Footer END -->
     </div>
     <!-- Body JS -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
     @yield('bodyJS')
 </body>
 </html>
--- resources/views/groups/edit.blade.php
+++ resources/views/groups/edit.blade.php
@@ -10,7 +10,11 @@
         {!! Form::model($group, ['method' => 'PATCH', 'action' => ['GroupController@update', $group->id]])!!}
 
         @include('groups._form', ['buttonLabel' => 'Update group'])
-        <a class="btn inline" href="">Delete group</a>
+        <a class="btn inline" data-method="delete" data-confirm="Are you sure?" href="{{url('groups/' . $group->id)}}">Delete group</a>
         {!! Form::close() !!}
     </div>
+@stop
+
+@section('bodyJS')
+    <script src="{{URL::asset('js/rest_link.js')}}"></script>
 @stop
